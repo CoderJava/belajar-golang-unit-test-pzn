5327f3fb7b3eb3d31c823064a91dc438f07c4383
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-05-27 08:39:51 +0700

    Table Test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -84,6 +84,36 @@ func TestSubTest(t *testing.T) {
 	})
 }
 
+func TestTableHelloWorld(t *testing.T) {
+	tests := []struct {
+		name     string
+		request  string
+		expected string
+	}{
+		{
+			name:     "Eko",
+			request:  "Eko",
+			expected: "Hello Eko",
+		},
+		{
+			name:     "Kurniawan",
+			request:  "Kurniawan",
+			expected: "Hello Kurniawan",
+		},
+		{
+			name:     "Khannedy",
+			request:  "Khannedy",
+			expected: "Hello Khannedy",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := HelloWorld(test.request)
+			require.Equal(t, test.expected, result, "Result must be '"+test.expected+"'")
+		})
+	}
+}
+
 // Cara 1 (nama function test-nya tidak ditampilkan)
 // cd helper
 // go test
